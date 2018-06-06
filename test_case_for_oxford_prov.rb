de2707a946720c494e49e5e1217051d1dc09e943
Benjamin Heller <echo29x@gmail.com>
2018-06-06 16:38:09 -0400

    secondary commit

--- test_case_for_oxford_prov.rb
+++ test_case_for_oxford_prov.rb
@@ -1,9 +1,11 @@
 require 'nokogiri'
 
-files = Dir['./medieval-mss/collections/Lyell/*.xml'].reject { |fn| fn.include? "empt" }
+# change to the correct path to the desired collection xml files...
+files = Dir['../medieval-mss/collections/Lyell/*.xml'].reject { |fn| fn.include? "empt" }
 
 files.each do |file|
   doc = Nokogiri::XML(open(file))
+
   if not doc.at("history")
     puts "No HISTORY tag, skipping:  #{file}"
   elsif doc.search("acquisition").count > 0
@@ -36,11 +38,6 @@ files.each do |file|
     doc.at('revisionDesc').prepend_child(change)
   end
 
-  # ... 
-  #
-
-  #puts doc.at('history').to_xml
-  
   fp = open(file, 'w')
   fp.puts doc.to_xml
   fp.close
